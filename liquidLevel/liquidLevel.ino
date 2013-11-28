bbc636bbbc4da2c63d01dadb54deb385eee17662
andreu <andreu@beta-robots.com>
2013-11-28 10:31:55 +0100

    Led level indicator added

--- liquidLevel/liquidLevel.ino
+++ liquidLevel/liquidLevel.ino
@@ -1,12 +1,14 @@
 
 //user parameters
-double lTh = 15; //length threshold to open electrovalve [cm] 
-int analogPin = 5; //analog pin where the sensor voltage divider is connected
 int nReadings = 10; //indicates how many sensor readings are used to compute a mean.
 boolean verbose = true; //when true, enables displaying debug info through serial port
 
+//arduino pins
+int analogPin = 5; //analog pin where the sensor voltage divider is connected
+int ledPins[] = {2,3,4}; //pins where leds are connected
+
 //parameters (electronics)
-double Vcc = 9.0; //power supply voltage [V]
+double Vcc = 9.25; //power supply voltage [V]
 double RL = 1500; //resistance of the voltage divider [ohm]
 double Rmin = 300;//min resistance value of eTape sensor [ohm]
 double lmax = 21.4; //total length of eTape sensor [cm]
@@ -22,6 +24,8 @@ double liquidLevelArray[10]; //an array of the last nReadings readings [cm]
 int arrayPt = 0; //indicates the index of liquidLevelArray to be set in the current iteration
 int ii; //iterator counter
 int jj; //array counter
+int nLeds;//number of leds in the bar
+int nLedsOn; //how many leds should be on
 
 //setup
 void setup() 
@@ -34,6 +38,13 @@ void setup()
       
       //sets pin as an input
       pinMode(analogPin, INPUT);
+
+      //sets number of leds from ledPins allocation and set led pins as outputs
+      nLeds = sizeof(ledPins)/2;
+      for (jj=0; jj<nReadings; jj++)
+      {
+            pinMode(ledPins[jj], OUTPUT); // initialize the digital pin as an output.
+      }
 }
  
 //main loop
@@ -53,30 +64,44 @@ void loop()
       //Entry to compute the mean , once we have the liquidLevelArray full 
       if (ii >= nReadings) 
       {
+            //compute mean
             liquidLevelMean = 0;
             for (jj=0; jj<nReadings; jj++)
             {
                   liquidLevelMean += liquidLevelArray[jj];
             }
             liquidLevelMean = liquidLevelMean/nReadings;
+            
+            //indcate with leds
+            nLedsOn = liquidLevelMean/lmax*3+1;
+            if (nLeds > nLeds) nLedsOn = nLeds;//check upper bound (in case of noisy readings)
+            if (nLeds < 0) nLedsOn = nLeds;//check upper bound (in case of noisy readings)
+            for (jj=0; jj<nLedsOn; jj++) digitalWrite(ledPins[jj], HIGH);
+            for (jj=nLedsOn; jj<nLeds; jj++) digitalWrite(ledPins[jj], LOW);
       }
+      
       
       //Serial messaging if verbose mode, each 10 iterations
       if ( (verbose) && (ii%10 == 0) )
       {
-            //Serial.print("analogLevel is ");//prints through serial port
-            //Serial.println(analogLevel, DEC);//prints through serial port
-            //Serial.print("analogVoltage is ");//prints through serial port
-            //Serial.println(analogVoltage, DEC);//prints through serial port
-            //Serial.print("liquidLevel is ");//prints through serial port
-            //Serial.println(liquidLevel, DEC);//prints through serial port
-            Serial.print("liquidLevelMean is ");//prints through serial port
-            Serial.println(liquidLevelMean, DEC);//prints through serial port
+            Serial.print("analogLevel: ");
+            Serial.println(analogLevel, DEC);
+            Serial.print("analogVoltage: ");
+            Serial.println(analogVoltage, DEC);
+            Serial.print("liquidLevel: ");
+            Serial.println(liquidLevel, DEC);
+            Serial.print("liquidLevelMean: ");
+            Serial.println(liquidLevelMean, DEC);
+            Serial.print("nLeds:  ");
+            Serial.println(nLeds, DEC);
+            Serial.print("nLedsOn:  ");
+            Serial.println(nLedsOn, DEC);
+            Serial.print("----------------------------\n\n");
       }
       
-      // wait for a second
+      // wait for 100 milliseconds
       delay(100); 
       
-      //increment iterator and array pointer;
+      //increment iterator;
       ii ++;
 }
