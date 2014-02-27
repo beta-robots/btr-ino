c4ca07888bb14a16998dd0f064cf05b1ba210e49
andreu <andreu@beta-robots.com>
2014-02-27 08:57:59 +0100

    Rotational velocity computation added

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -11,27 +11,54 @@
 // However, even if you are not forced, it would be nice to hear from you, specially if you find bugs or you improve something :-)
 //
 
+//ENCODER PARAMETERS
+int ppr = 360; //pulses per revolution
+
 //main variables
-volatile long pulses = 0;
-volatile int encoderBpin = 3;
+volatile int encoderBpin = 3; //pin where encoder B signal is attached
+volatile long pulses = 0; //encoder pulse counter
+long p0 = 0; //last value for pulses
+long p1 = 0; //current value for pulses
+int t0 = 0; //last time measurement
+int t1 = 0; //current time measurement
+double vRot = 0; //rotational velocity in degrees/s 
 
 void setup()
 {
-      // opens serial port, sets data rate to 9600 bps
+      //opens serial port, sets data rate to 9600 bps
       Serial.begin(9600);
       
       //attach interrupts to A and B encoder signals
       attachInterrupt(0, encoderAinterrupt, RISING);
+      
+      //init t0
+      t0 = millis();
 }
 
 void loop()
-{
-      //Output current pulses
-      Serial.print("pulses: ");
-      Serial.println(pulses, DEC);
-      
+{      
       // wait for 1/5 second
       delay(200);       
+      
+      //get time now
+      t1 = millis();
+      
+      //get number of pulses from interrupt counter (should be protected by a kind of mutex !)
+      p1 = pulses;
+      
+      //compute vRot
+      vRot = ((double)(p1-p0)/(double)(t1-t0))*1000*(360.0/(double)ppr);
+      
+      //keep current values
+      t0 = t1;
+      p0 = p1;
+      
+      //Output rotational velocity value
+      Serial.print(t1,DEC);
+      Serial.print(" ");
+      Serial.print(p1,DEC);
+      Serial.print(" ");
+      Serial.println(vRot, DEC);
 }
 
 void encoderAinterrupt()
