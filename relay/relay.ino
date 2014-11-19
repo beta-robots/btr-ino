d73148f11b895a97b34259d69ccf99ce364beb21
andreu <andreu@beta-robots.com>
2014-11-19 10:46:20 +0100

    adding relay project

---
+++ relay/relay.ino
@@ -0,0 +1,22 @@
+
+// Pin 13 has a LED connected on most Arduino boards.
+int relayPin = 12;
+
+//setup
+void setup() 
+{
+    //sets relayPin as an output
+    pinMode(relayPin, OUTPUT);  
+}
+ 
+//main loop
+void loop() 
+{
+    // switch relay and wait
+    digitalWrite(relayPin, HIGH);   
+    delay(3000);    
+      
+    // switch relay and wait
+    digitalWrite(relayPin, LOW);
+    delay(3000);     
+}
