b73e5bd2c0700ff4f694c056b233124a15ca4f37
andreucm <andreu@beta-robots.com>
2016-04-13 12:54:36 +0200

    Minor changes and added variables instead of hardcoded magic numbers

--- dc_motor_controller/dc_motor_controller.ino
+++ dc_motor_controller/dc_motor_controller.ino
@@ -2,9 +2,12 @@
 
 //pin assignement
 int pin_led = 13; // Pin 13 has a LED connected on most Arduino boards.
-int pin_motor_controller_e1 = 3; //pwm
-int pin_motor_controller_m1 = 2; //turn direction
+int pin_motor_controller_e2 = 3; //pwm
+int pin_motor_controller_m2 = 2; //turn direction
 unsigned long motor_iteration_delta_t = 200; //duration of each iteration [milliseconds]
+int pwm_min = 100; 
+int pwm_max = 200; 
+int pwm_step = 2; 
 
 //setup
 void setup() 
@@ -13,9 +16,16 @@ void setup()
     pinMode(pin_led, OUTPUT);
     
     //sets pin_motor_enable as a digital output
-    pinMode(pin_motor_controller_m1, OUTPUT);  
+    pinMode(pin_motor_controller_m2, OUTPUT);  
 }
- 
+
+//stops a motor at a given pin 
+void stop(int _pin)
+{
+    //just send a PWM 0% at the pin
+    analogWrite(_pin, 0);   
+}
+
 //ramp-up/down pwm to a given pin
 void pwmRamp(int _pin, int _pwm_init, int _pwm_end, int _pwm_delta, unsigned long _delay)
 {
@@ -56,16 +66,18 @@ void loop()
     digitalWrite(pin_led, HIGH);   
  
     //set motor direction
-    digitalWrite(pin_motor_controller_m1, HIGH);   
+    digitalWrite(pin_motor_controller_m2, HIGH);   
 
     //ramp-up
-    pwmRamp(pin_motor_controller_e1,100,200,2,motor_iteration_delta_t);
+    pwmRamp(pin_motor_controller_e2,pwm_min,pwm_max,pwm_step,motor_iteration_delta_t);
     
     //ramp-down
-    pwmRamp(pin_motor_controller_e1,200,100,-2,motor_iteration_delta_t);
+    pwmRamp(pin_motor_controller_e2,pwm_max,pwm_min,-pwm_step,motor_iteration_delta_t);
 
+    //stop
+    stop(pin_motor_controller_e2);
 
-    //breathe
+    //breath
     delay(200);
     
 //2n cycle
@@ -73,11 +85,18 @@ void loop()
     digitalWrite(pin_led, LOW);   
     
     //change motor direction
-    digitalWrite(pin_motor_controller_m1, LOW);   
+    digitalWrite(pin_motor_controller_m2, LOW);   
     
     //ramp-up
-    pwmRamp(pin_motor_controller_e1,100,200,2,motor_iteration_delta_t);
+    pwmRamp(pin_motor_controller_e2,pwm_min,pwm_max,pwm_step,motor_iteration_delta_t);
     
     //ramp-down
-    pwmRamp(pin_motor_controller_e1,200,100,-2,motor_iteration_delta_t);
+    pwmRamp(pin_motor_controller_e2,pwm_max,pwm_min,-pwm_step,motor_iteration_delta_t);
+          
+    //stop
+    stop(pin_motor_controller_e2);
+
+    //breath
+    delay(200);
+
 }
